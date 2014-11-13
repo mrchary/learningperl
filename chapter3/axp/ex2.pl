0e2bfba41b655a60d0c632b3faa2a929c9af0e61
Alex Panayotopoulos <alex.panayotopoulos@datacash.com>
2014-11-13 08:33:33 +0000

    Fixing off-by-one error in ex2, and shortening ex3

--- chapter3/axp/ex2.pl
+++ chapter3/axp/ex2.pl
@@ -5,5 +5,5 @@ use 5.010;
 my @names = qw( fred betty barney dino wilma pebbles bamm-bamm );
 
 foreach ( <STDIN> ) {
-  say $names[$_] if ( $_ >= 0 and $_ < @names );
+  say $names[$_ - 1] if ( $_ > 0 and $_ <= @names );
 }
--- chapter3/axp/ex3.pl
+++ chapter3/axp/ex3.pl
@@ -2,8 +2,7 @@ use strict;
 use warnings;
 use 5.010;
 
-my @input = <STDIN>;
-@input = sort @input;
+my @input = sort <STDIN>;
 print @input; # One on each line
 chomp ( @input );
 say "@input"; # All on the same line

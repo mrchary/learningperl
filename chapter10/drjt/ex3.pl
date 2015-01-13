dc6cec7c6ddd833f13230dfe5e483bce3448c181
David Thrussell <david.thrussell@datacash.com>
2015-01-13 09:09:35 +0000

    removed old question from ex3

--- chapter10/drjt/ex3.pl
+++ chapter10/drjt/ex3.pl
@@ -3,13 +3,6 @@ use 5.010;
 use warnings;
 use strict;
 
-# Write a program to list all of the keys and values in %ENV. Print the
-# results in two columns in ASCIIbetical order. For extra credit, arrange
-# the output to vertically align both columns. The length function can help
-# you figure out how wide to make the first column. Once you get the program
-# running, try setting some new environment variables and ensuring that they
-# show up in your output.
-
 sub maxlength {
   my $max = length shift @_;
   foreach ( @_ ) {
